1416a817c57381e7a35bd9741e211fb2e5c5247c
Subram Panaiyadiyan <38381153+psubrambe@users.noreply.github.com>
2019-10-22 10:37:16 +0100

    Update Actions.js

--- tests/wdio/pages/common/Actions.js
+++ tests/wdio/pages/common/Actions.js
@@ -12,22 +12,7 @@ class Actions {
 
     setValue(locator, value) {
         locator = Page.getElements(locator);
-        if (value === 'randomName') {
-            let randomName = faker.name.firstName();
-            if (randomName.includes('\'')) {
-                randomName = randomName.replace(/\'/g, '');
-            }
-            let lastName = faker.name.lastName();
-            if (lastName.includes('\'')) {
-                lastName = lastName.replace(/\'/g, '');
-            }
-            let value = randomName + lastName;
-            if (value.length > 20) {
-                value = value.substring(0, 15);
-            }
-            $(locator).waitForDisplayed(50000, false, 'Element ' + locator + ' not found');
-            return $(locator).setValue(value);
-        } else if (value === 'password') {
+        if (value === 'password') {
             $(locator).waitForDisplayed(50000, false, 'Element ' + locator + ' not found');
             return $(locator).setValue(userDetails.password);
         } else {
@@ -67,45 +52,12 @@ class Actions {
         return browser.pause(seconds * 1000);
     }
 
-    isExisting(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).isExisting();
-    }
-
-    getText(locator) {
-        locator = Page.getElements(locator);
-        $(locator).waitForDisplayed(50000, false, 'Element ' + locator + ' not found');
-        return $(locator).getText();
-    }
-
-    selectByAttribute(locator, attribute, value) {
-        locator = Page.getElements(locator);
-        $(locator).waitForDisplayed(50000, false, 'Element ' + locator + ' not found');
-        return $(locator).selectByAttribute(attribute, value);
-    }
-
     getUrl() {
         return browser.getUrl();
     }
 
-    isVisible(locator) {
-        locator = Page.getElements(locator);
-        $(locator).waitForDisplayed(50000, false, 'Element ' + locator + ' not found');
-        let value = $(locator).isExisting();
-        if (value) {
-            return assert.equal(value, true, 'Element Present');
-        } else {
-            return assert.equal(value, true, 'Unable to locate element');
-        }
-    }
-
     refresh() {
         return browser.refresh();
-    }
-
-    waitForExist(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).waitForExist(50000, false, 'Element ' + locator + ' not found');
     }
 
     accountCredentials(username, password, brand) {
@@ -113,49 +65,9 @@ class Actions {
         userDetails['password'] = password;
         userDetails['brand'] = brand;
     }
-
-    getAttribute(attribute, locator) {
-        locator = Page.getElements(locator);
-        return $(locator).getAttribute(attribute);
-    }
-
-    selectByVisibleText(locator, value) {
-        locator = Page.getElements(locator);
-        return $(locator).selectByVisibleText(value);
-    }
-
-    selectByIndex(locator, index) {
-        locator = Page.getElements(locator);
-        return $(locator).selectByIndex(index);
-    }
-
+    
     getUserDetail() {
         return userDetails;
-    }
-
-    clearValue(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).clearValue();
-    }
-
-    isEnabled(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).isEnabled();
-    }
-
-    waitForEnabled(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).waitForEnabled(20000, false, 'Unable to perform action as element is not enabled');
-    }
-
-    waitForDisappear(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).waitForDisplayed(320000, true, 'Element ' + locator + ' still located in page');
-    }
-
-    scroll(locator) {
-        locator = Page.getElements(locator);
-        return $(locator).scrollIntoView();
     }
 
     waitUntil(locator, attribute, value) {
